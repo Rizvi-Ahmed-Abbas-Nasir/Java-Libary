fbe3e173c4dcf7c9dabb15e84bb767560e3d22a9
Rizvi-Ahmed-Abbas_Nasir <abbasrizvi200209@gmail.com>
2022-02-27 01:58:52 +0530

    Abbas

---
+++ javaExample.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class javaExample {
+    public static void main (String[] args){
+        Scanner scn = new Scanner(System.in);
+        int[] array = {55,34,434,535};
+        for(int i = 0; i < array.length; i++)
+        {
+            System.out.print(array[i]);
+            System.out.print(" ");
+        }
+
+        System.out.println(" ");
+
+        String[] arrayOfStrings = {"Abbas","Rizvi","Ahemd"};
+        for(int i = 0; i < arrayOfStrings.length; i++)
+        {
+            System.out.print(arrayOfStrings[i]);
+            System.out.print(" ");
+        }
+
+    scn.close();
+    }
+
+    
+
+}
